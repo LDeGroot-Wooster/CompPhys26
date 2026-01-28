007b82507411cb4bd675bf3a1d1703dcdb7a0278
LDeGroot-Wooster <ldegroot@wooster.edu>
2026-01-28 11:01:08 -0500

    adding files for class Wed, Jan 28

---
+++ tests/test_Lab3.py
@@ -0,0 +1,78 @@
+# import numpy
+import pytest
+from testbook import testbook
+
+#@testbook('/Users/ldegroot/CompPhys26/Labs/LabAssignment3_S26.ipynb', execute=True)
+#@testbook('/Users/ldegroot/OneDrive - The College of Wooster/a_PHYS230_S26/Labs/LabAssignment3_Solutions_S26.ipynb', execute=True)
+#@testbook('/path/to/notebook.ipynb', execute=True)
+
+@pytest.fixture(scope='module')
+def tb():
+    with testbook('/Users/ldegroot/OneDrive - The College of Wooster/a_PHYS230_S26/Labs/LabAssignment3_Solutions_S26.ipynb', execute=True) as tb:
+        yield tb
+
+def test_first_error_cell(tb):
+    # Execute specific cell
+    #tb.execute_cell('Error1')
+    #out_vals = tb.cell_output('Error1')
+    out_val = tb.execute_cell('Error1')
+    #out_vals = tb.execute_cell()
+
+    # assert the output 
+    output_list = out_val["outputs"][0]["text"].splitlines()
+    assert len(output_list) == 10
+    assert float(output_list[-1]) == 9
+
+
+def test_second_error_cell(tb):
+    # Execute specific cell
+    #tb.execute_cell('Error1')
+    #out_vals = tb.execute_cell('Error2')
+    tb.execute_cell('Error2')
+
+  
+
+def test_third_error_cell(tb): 
+    tb.execute_cell('Error3')
+    #result = tb.execute_cell('Error3')
+    calc = tb.ref("calculate")
+    assert calc(5) == -50
+
+
+def test_fourth_error_cell(tb): 
+    tb.execute_cell('Error4')
+    #result = tb.execute_cell()
+    #assert result[0] > 0
+    #assert result[1] < 0
+    quad = tb.ref("quadratic")
+    assert round(quad(2,6,-35)[0],3) == 2.944
+
+
+
+def test_first_snippet(tb):
+    out_val = tb.execute_cell('Snippet1')
+
+    # assert the output 
+    output_list = out_val["outputs"][0]["text"].splitlines()
+    assert len(output_list) == 13
+    assert (output_list[-1]) == 's'
+
+
+
+def test_second_snippet(tb):
+    tb.execute_cell('Snippet2')
+    
+    
+
+def test_third_snippet(tb):
+    tb.execute_cell('Snippet3')
+
+
+
+def test_fourth_snippet(tb):
+    tb.execute_cell('Snippet4')
+    #result = tb.execute_cell()
+    magnitude = tb.ref("mag")
+    assert round(magnitude(38.5),3) == 21.996
+
+
